776bb094fe42f323c3bae55756ba645b3576f007
Danang Estutomoaji <ahmad.candra.17@gmail.com>
2022-05-31 20:32:19 +0700

    Update Edit Tickets Technician

--- app/Http/Controllers/TechnicianTicketController.php
+++ app/Http/Controllers/TechnicianTicketController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Ticket;
 use Yajra\Datatables\Datatables;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Auth;
 
 class TechnicianTicketController extends Controller
