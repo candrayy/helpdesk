2dbe6b9a146d04f8c9efa1171bba4e76cf369765
Danang Estutomoaji <ahmad.candra.17@gmail.com>
2022-06-05 14:59:16 +0700

    Update Container Dashboard Admin

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -21,6 +21,34 @@
         </div>
       </div>
     </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-header">
+            Create Ticket
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-header">
+            Create Account User
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   </div>
 
 <!-- End Konten -->
